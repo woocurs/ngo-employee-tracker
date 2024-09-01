036ddbc586b845d2e29fa2d0e3dff2e995b80da8
KamsikaJ <Kamsika@woocurs.com>
2024-09-01 21:17:42 +0530

    folder name changed

--- employee_location_tracking_thiva/footer.php
+++ employee_location_tracking_thiva/footer.php
@@ -1,2 +1,6 @@
+<footer style="text-align: center; padding: 10px; background-color: #1d2630;"> 
+    <p>&copy; <?php echo date("Y"); ?> NGO. All Rights Reserved.</p>
+</footer>
+        
 </body>
 </html>
--- employee_location_tracking_thiva/login.php
+++ employee_location_tracking_thiva/login.php
@@ -1,4 +1,5 @@
-<?php include 'header.php'; ?>
+<?php include('header.php'); include('footer.php'); ?>
+
 
 <?php
 //login.php
