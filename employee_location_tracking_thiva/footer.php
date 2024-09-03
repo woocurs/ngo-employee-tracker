10a224c5adba168d797aef2943e8b71865fd960a
Thiva-Dva <Thivagini@woocurs.com>
2024-09-03 14:15:32 +0530

    footer updated

--- employee_location_tracking_thiva/footer.php
+++ employee_location_tracking_thiva/footer.php
@@ -1,2 +1,19 @@
+<footer class="bg-dark text-white text-center py-3 mt-auto">
+    <div class="container">
+        <p class="mb-1">&copy; <?php echo date("Y"); ?> NGO Employee Location Tracking System. All rights reserved.</p>
+        <p class="mb-0">
+            Follow us on:
+            <a href="https://www.facebook.com" target="_blank" class="text-white ms-2">Facebook</a> |
+            <a href="https://www.twitter.com" target="_blank" class="text-white ms-2">Twitter</a> |
+            <a href="https://www.linkedin.com" target="_blank" class="text-white ms-2">LinkedIn</a>
+        </p>
+    </div>
+</footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- employee_location_tracking_thiva/register.php
+++ employee_location_tracking_thiva/register.php
@@ -43,3 +43,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </body>
 
 </html>
+<?php include ('footer.php'); ?>
